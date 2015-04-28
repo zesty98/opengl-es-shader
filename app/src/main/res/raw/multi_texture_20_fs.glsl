ae821e14d6c71204d4928b64bd243386c88329e9
gomdev <gomdev@naver.com>
2015-04-28 22:15:15 +0900

    modify multi_texture_20_fs.glsl to simplify

--- app/src/main/res/raw/multi_texture_20_fs.glsl
+++ app/src/main/res/raw/multi_texture_20_fs.glsl
@@ -8,14 +8,11 @@ uniform int uUseMultiTexture;
 
 void main() {
     vec4 dst = texture2D(uTexture, vTexCoord);
-    float dstAlpha = dst.a;
     vec4 src = texture2D(uTexture2, vTexCoord);
-    float srcAlpha = src.a;
-
     vec4 color;
 
     if (uUseMultiTexture == 1) {
-        color = src * srcAlpha + dst * (1.0 - srcAlpha);
+        color = src * src.a + dst * (1.0 - src.a);
     } else {
         color = dst;
     }
